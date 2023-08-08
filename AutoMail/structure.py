08519e1125c100bb846c0f895f1d4dae7eb07bc0
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-08-07 22:56:03 -0400

    Se ingreso smtplib, env

---
+++ AutoMail/structure.py
@@ -0,0 +1,25 @@
+title_structure = """"Asunto: Respuesta a su consulta / Solicitud de información"""
+
+body_structure = """Estimado/a [Nombre del Cliente/Formal]:
+
+Espero que este correo lo encuentre bien. En nombre de [Nombre de la Institución/Compañía], me complace responder a su consulta / solicitud de información que recibimos recientemente.
+
+Antes que nada, queremos expresar nuestro agradecimiento por su interés en nuestros productos / servicios y por tomar el tiempo para ponerse en contacto con nosotros.
+
+[En esta sección, es importante ser claro y conciso al proporcionar la información solicitada por el cliente. Si es necesario, incluya detalles técnicos o aspectos específicos relacionados con la consulta del cliente. Si la consulta requiere investigación adicional, asegúrese de mencionar que están trabajando en ello y proporcionará una respuesta completa lo antes posible.]
+
+Si tiene alguna otra pregunta o inquietud, no dude en hacérnoslo saber. Estamos aquí para ayudarle en todo lo que podamos.
+
+[Si corresponde, agregue una breve mención sobre la política de privacidad o términos y condiciones aplicables.]
+
+Nuevamente, agradecemos su interés en [Nombre de la Institución/Compañía] y esperamos poder servirle de la mejor manera posible. Si necesita más asistencia, no dude en comunicarse con nuestro equipo de soporte.
+
+Atentamente,
+
+[Su Nombre]
+[Su Cargo/Posición]
+[Nombre de la Institución/Compañía]
+[Teléfono de Contacto]
+[Correo Electrónico de Contacto]"""
+
+
